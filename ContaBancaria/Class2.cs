a77919b6a29c7165b70b8f35df6a68ca7ef4f62e
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-11-28 18:50:33 -0300

    Adicionar arquivos de projeto.

---
+++ ContaBancaria/Class2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContaBancaria
+{
+
+    class ContaJ : Conta
+    {
+        public double EmprestimoLimite { get; set; } // Declaração das variáveis e encapsulamento
+
+        public ContaJ()
+        {
+            //Construtor padrão
+        }
+        public ContaJ(int numero, string titular, double saldo, double emprestimoLimite) : base(numero, titular, saldo) //Construtor com parâmetros
+        {
+            EmprestimoLimite = emprestimoLimite;
+        }
+        public void Emprestimo(double saldoTotal) //Realiza Empréstimo
+        {
+            if (saldoTotal <= EmprestimoLimite)
+            {
+                Saldo += saldoTotal;
+            }
+        }
+    }
+}
---
+++ ContaBancaria/Class3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContaBancaria
+{
+    class ContaP : Conta
+    {
+        public double TaxaJuros { get; set; } //Declaração das variáveis e encapsulamento
+        public ContaP()
+        {
+            //Construtor padrão
+        }
+        public ContaP(int numero, string titular, double saldo, double taxaJuros) : base(numero, titular, saldo) //Construtor com parâmetros
+        {
+            TaxaJuros = taxaJuros;
+        }
+        public sealed override void Saque(double saldoTotal) //Realiza saque
+        {
+            base.Saque(saldoTotal);
+            Saldo -= 2.0;
+        }
+        public void atualizacaoSaldo() //Atualiza o Saldo
+        {
+            Saldo += Saldo * TaxaJuros;
+
+        }
+    }
+}
